76f85b16f612857d187a8b767214cc388e94b000
Cxatek18 <griga2888@mail.ru>
2024-06-05 17:56:54 +0500

    #develop - переименовал директорию с тестами

--- app/src/androidTest/java/ru/it_cron/intern3/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/it_cron/intern3/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.team.itcron", appContext.packageName)
+        assertEquals("ru.it_cron.intern3", appContext.packageName)
     }
 }
